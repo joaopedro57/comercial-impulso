88e77b28e5f951e9deaa90f26d18e7c69e28d586
João Pedro <joaopedro@solides.com.br>
2020-04-17 16:44:22 -0300

    CORS

--- application/controllers/Api_controller.php
+++ application/controllers/Api_controller.php
@@ -1,17 +1,18 @@
 <?php
- /* CORS Joy!!! */
-  header('Access-Control-Allow-Origin: *');
-  header('Access-Control-Allow-Headers: Content-Length');
-  header('Access-Control-Expose-Headers: Content-Length');
-  header('Timing-Allow-Origin: *');
+/* CORS Joy!!! */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Length');
+header('Access-Control-Expose-Headers: Content-Length');
+header('Timing-Allow-Origin: *');
 
   /* anything.. really... */
-  header('Content-Type: text/json;');
-  header('X-Powered-By: peanut-butter');
+header('Content-Type: text/json;');
+header('X-Powered-By: peanut-butter');
 
 defined('BASEPATH') OR exit('No direct script access allowed');
+require(APPPATH.'libraries/REST_Controller.php');
 
-class Api_controller extends CI_Controller {
+class Api_controller extends REST_Controller {
 	public function __construct()
 	{
           parent::__construct();
@@ -21,18 +22,19 @@ class Api_controller extends CI_Controller {
 
 	public function cadastrar_lead_post()
 	{
-		$dados = $this->input->post();
-		print_r($dados);
+		$dados = $this->post();
+		print_r($dados);exit;
+
 		$dados = array(
-			'firstName' => $dados['firstame'],
-			'name' => $dados['firstame'],
-			'email' => $dados['email'],
-			'company' => $dados['company'],
+			'firstName' => "Lucas",
+			'name' => "Lucas Doido",
+			'email' => "lucas@impulso.work",
+			'company' => "Impulso",
 			'phones' => array( array(
 				'label' => "Escritório",
-				'phone' => "+55".$dados['phone'],
+				'phone' => "+5531992280009",
 				'lastUsage' => "2019-05-16T20:00:00Z")));
-		print_r($dados);exit;
+
 		$meetime = json_encode($dados);
 		$enviar = meetime_lead($meetime);
 
