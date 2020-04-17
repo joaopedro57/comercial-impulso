2c1e41a497368a9451aaaac293d2eead4fb920c1
João Pedro <joaopedro@solides.com.br>
2020-04-17 12:32:02 -0300

    app.yaml 2

--- application/controllers/Api_controller.php
+++ application/controllers/Api_controller.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Api_controller extends CI_Controller {
