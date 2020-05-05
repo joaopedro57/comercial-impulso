ab613f7856c169eb14ad92c75a54056645ed1600
João Pedro <joaopedro@solides.com.br>
2020-05-05 12:38:18 -0300

    vai

--- application/controllers/Api_controller.php
+++ application/controllers/Api_controller.php
@@ -24,7 +24,10 @@ class Api_controller extends REST_Controller {
 	public function cadastrar_lead_post()
 	{
 		$dados = $this->post();
-		$this->Meetime_models->exemplo(array('ex' => $dados));
+		$ex = array(
+			'ex' => json_encode($dados));
+
+		$this->Meetime_models->exemplo($ex);
 		$firstName = explode(" ", $dados['dataForm']['2']['value']);
 		$phone = soNumero($dados['dataForm']['4']['value']);
 
